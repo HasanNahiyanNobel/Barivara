5fa4b5249e7dc830bfc4c67cfa47575bf3f348e7
Hasan Nahiyan Nobel <bilbilu.ugaccount@users.noreply.github.com>
2019-07-05 12:28:56 +0600

    First commit

---
+++ app/src/main/java/com/example/nobeltest5/LandlordPage1.java
@@ -0,0 +1,17 @@
+package com.example.nobeltest5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandlordPage1 extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.landlord_page_1);
+	}
+
+	public void button3Clicked() {
+
+	}
+}
---
+++ app/src/main/java/com/example/nobeltest5/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.nobeltest5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+	Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		listenButtons();
+	}
+
+	public void listenButtons() {
+		final Context CONTEXT = this;
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(CONTEXT, LandlordPage1.class);
+				startActivity(intent);
+			}
+		});
+	}
+}
