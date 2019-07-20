a9949f089e93b95c068f1ea14185f591959ecd9e
Hasan Nahiyan Nobel <bilbilu.ugaccount@users.noreply.github.com>
2019-07-20 16:03:13 +0600

    Changed version of gms:play-services-maps.

--- app/src/main/java/com/example/barivara/LandlordMainPage.java
+++ app/src/main/java/com/example/barivara/LandlordMainPage.java
@@ -18,7 +18,7 @@ public class LandlordMainPage extends AppCompatActivity {
 	}
 
 	public void addANewHouse (View view) {
-		Intent intent = new Intent(this, LandlordHouseAddress_Old.class);
+		Intent intent = new Intent(this, MapsActivity.class);
 		startActivity(intent);
 	}
 }
