1930b730a41d7f1e535d2175fe052c04e3aa7a6e
Hasan Nahiyan Nobel <bilbilu.ugaccount@users.noreply.github.com>
2019-07-21 11:44:54 +0600

    Changed a data structure.

--- app/src/main/java/com/example/barivara/LandlordHouseAddress.java
+++ app/src/main/java/com/example/barivara/LandlordHouseAddress.java
@@ -1,39 +1,53 @@
 package com.example.barivara;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class LandlordHouseAddress extends AppCompatActivity {
 	AutoCompleteTextView actvDistricts, actvUpazila, actvArea;
 
-	private String[] nameOfDivisions;
+	private ArrayList<String> nameOfDivisions = new ArrayList<String>();
 
-	private static String[] nameOfDistricts = new String[]{"ঢাকা","নারায়ণগঞ্জ","গাজীপুর","মুন্সীগঞ্জ","মানিকগঞ্জ","নরসিংদী","ফরিদপুর","মাদারীপুর","গোপালগঞ্জ","রাজবাড়ী","শরীয়তপুর","টাঙ্গাইল","কিশোরগঞ্জ"};
+	private ArrayList<String> nameOfDistricts = new ArrayList<String>();
 
-	private static String[] nameOfUpazilas = new String[]{"ঢাকা","ধামরাই","দোহার","কেরানীগঞ্জ","নবাবগঞ্জ","সাভার"};
+	private ArrayList<String> nameOfUpazilas = new ArrayList<String>();
 
-	private static String[] nameOfAreas;
+	private ArrayList<String> nameOfAreas = new ArrayList<String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_landlord_house_address);
 
+		nameOfDistricts.addAll(Arrays.asList("ঢাকা","নারায়ণগঞ্জ","গাজীপুর","মুন্সীগঞ্জ","মানিকগঞ্জ","নরসিংদী","ফরিদপুর","মাদারীপুর","গোপালগঞ্জ","রাজবাড়ী","শরীয়তপুর","টাঙ্গাইল","কিশোরগঞ্জ"));
+
 		actvDistricts = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
 		ArrayAdapter<String> districtsArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,nameOfDistricts);
 		actvDistricts.setAdapter(districtsArrayAdapter);
 		actvDistricts.setThreshold(1);
+
+		nameOfUpazilas.addAll(Arrays.asList("ঢাকা","ধামরাই","দোহার","নবাবগঞ্জ","কেরানীগঞ্জ","সাভার"));
 
 		actvUpazila = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
 		ArrayAdapter<String> upazilasArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,nameOfUpazilas);
 		actvUpazila.setAdapter(upazilasArrayAdapter);
 		actvUpazila.setThreshold(1);
 
-		nameOfAreas = new String[]{"মোহাম্মাদপুর","খিলগাঁও","উত্তরা","পলাশী"};
+		nameOfAreas.addAll(Arrays.asList("মোহাম্মাদপুর","খিলগাঁও","উত্তরা","পলাশী","শাঁখারী বাজার"));
 
 		actvArea = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView3);
 		ArrayAdapter<String> areasArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, nameOfAreas);
@@ -51,5 +65,32 @@ public class LandlordHouseAddress extends AppCompatActivity {
 
 	public void showDropdownOfAreas(View view) {
 		actvArea.showDropDown();
+	}
+
+	private String readFromFile(Context context) {
+		String ret = "";
+		try {
+			InputStream inputStream = context.openFileInput("districts.txt");
+
+			if ( inputStream != null ) {
+				InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+				String receiveString = "";
+				StringBuilder stringBuilder = new StringBuilder();
+
+				while ( (receiveString = bufferedReader.readLine()) != null ) {
+					stringBuilder.append(receiveString);
+				}
+
+				inputStream.close();
+				ret = stringBuilder.toString();
+			}
+		}
+		catch (FileNotFoundException e) {
+			Log.e("Locations of Bangladesh","File not found: " + e.toString());
+		} catch (IOException e) {
+			Log.e("Locations of Bangladesh", "Can not read file: " + e.toString());
+		}
+		return ret;
 	}
 }
