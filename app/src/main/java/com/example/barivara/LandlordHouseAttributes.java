a824552536564ea37bd6e8b98a471ab0ee6f7d90
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-08-17 15:00:44 +0600

    Added rent amounts' page.

--- app/src/main/java/com/example/barivara/LandlordHouseAttributes.java
+++ app/src/main/java/com/example/barivara/LandlordHouseAttributes.java
@@ -1,6 +1,8 @@
 package com.example.barivara;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class LandlordHouseAttributes extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_landlord_house_attributes);
+	}
+
+	public void goToRentAmount(View view) {
+		Intent intent = new Intent(this,LandlordRentAmount.class);
+		startActivity(intent);
 	}
 }
---
+++ app/src/main/java/com/example/barivara/LandlordRentAmount.java
@@ -0,0 +1,24 @@
+package com.example.barivara;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandlordRentAmount extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_landlord_rent_amount);
+	}
+
+	public void finishAddingHouse(View view) {
+		/**
+		 * Finishes the proccess of adding a house, and goes to Landlord's main page.
+		 */
+		Intent intent = new Intent(this,LandlordMainPage.class);
+		startActivity(intent);
+	}
+}
