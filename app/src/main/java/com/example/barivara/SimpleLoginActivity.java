74565e7cd3d9f286c31d5c934f89994fb3fe4b76
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-01 13:14:24 +0600

    Added simple login activity.

---
+++ app/src/main/java/com/example/barivara/SimpleLoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.barivara;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimpleLoginActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_simple_login);
+	}
+
+	public void doLoginOrRegister(View view) {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+}
