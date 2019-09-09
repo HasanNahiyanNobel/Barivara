5f82fb362682fe3457eef142f0ca9caa968faec5
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-09 15:05:11 +0600

    Retrieved registration form texts.

--- app/src/main/java/com/example/barivara/RegistrationActivity.java
+++ app/src/main/java/com/example/barivara/RegistrationActivity.java
@@ -3,10 +3,13 @@ package com.example.barivara;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationActivity extends AppCompatActivity {
+	EditText nameEditText, emailEditText, passwordEditText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,13 @@ public class RegistrationActivity extends AppCompatActivity {
 	}
 
 	public void register(View view) {
+		nameEditText = findViewById(R.id.editText5);
+		emailEditText = findViewById(R.id.editText7);
+		passwordEditText = findViewById(R.id.editText8);
+		Toast.makeText(RegistrationActivity.this, nameEditText.getText(), Toast.LENGTH_SHORT).show();
+		Toast.makeText(RegistrationActivity.this, emailEditText.getText(), Toast.LENGTH_SHORT).show();
+		Toast.makeText(RegistrationActivity.this, passwordEditText.getText(), Toast.LENGTH_SHORT).show();
+
 		Intent intent = new Intent(this, HomeActivity.class);
 		startActivity(intent);
 	}
