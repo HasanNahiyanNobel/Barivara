2ee12a93aca3d656b1f5cf5cc9bb80729064bb04
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-09 15:18:14 +0600

    Tried to post data.

--- app/src/main/java/com/example/barivara/RegistrationActivity.java
+++ app/src/main/java/com/example/barivara/RegistrationActivity.java
@@ -8,6 +8,17 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.barivara.api.User;
+import com.example.barivara.api.UserClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RegistrationActivity extends AppCompatActivity {
 	EditText nameEditText, emailEditText, passwordEditText;
 
@@ -21,9 +32,27 @@ public class RegistrationActivity extends AppCompatActivity {
 		nameEditText = findViewById(R.id.editText5);
 		emailEditText = findViewById(R.id.editText7);
 		passwordEditText = findViewById(R.id.editText8);
-		Toast.makeText(RegistrationActivity.this, nameEditText.getText(), Toast.LENGTH_SHORT).show();
-		Toast.makeText(RegistrationActivity.this, emailEditText.getText(), Toast.LENGTH_SHORT).show();
-		Toast.makeText(RegistrationActivity.this, passwordEditText.getText(), Toast.LENGTH_SHORT).show();
+
+		Retrofit.Builder builder = new Retrofit.Builder()
+				.baseUrl(getString(R.string.server_and_port))
+				.addConverterFactory(GsonConverterFactory.create());
+		Retrofit retrofit = builder.build();
+		UserClient userClient = retrofit.create(UserClient.class);
+		Call<List<User>> userListCall = userClient.userAll();
+		userListCall.enqueue(new Callback<List<User>>() {
+			@Override
+			public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+				List<User> userList = response.body();
+				for(User user : userList){
+					//Toast.makeText(FindAHouse.this, house.getElaka(), Toast.LENGTH_SHORT).show();
+					//placeName.add(house.getElaka()+" ("+house.getUpazilla()+", "+house.getZilla()+")");
+				}
+			}
+			@Override
+			public void onFailure(Call<List<User>> call, Throwable t) {
+				Toast.makeText(RegistrationActivity.this, "error :(", Toast.LENGTH_SHORT).show();
+			}
+		});
 
 		Intent intent = new Intent(this, HomeActivity.class);
 		startActivity(intent);
