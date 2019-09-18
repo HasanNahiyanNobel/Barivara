03e15820e2122a729683003aa26bb57d14ea2826
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-18 11:20:13 +0600

    Read barivara.

--- app/src/main/java/com/example/barivara/landlord/RentAmount.java
+++ app/src/main/java/com/example/barivara/landlord/RentAmount.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,8 @@ import com.example.barivara.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class RentAmount extends AppCompatActivity {
+	EditText barivataEditText;
+
 	@Override
 	protected void attachBaseContext(Context newBase) {
 		super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
@@ -21,13 +24,20 @@ public class RentAmount extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_landlord_rent_amount);
+		initializeViews();
 	}
 
 	public void finishAddingHouse(View view) {
 		/**
 		 * Finishes the proccess of adding a house, and goes to Landlord's main page.
 		 */
+		HouseAddress.newHouse.setBarivara(Integer.parseInt(barivataEditText.getText().toString()));
+
 		Intent intent = new Intent(this, MainPage.class);
 		startActivity(intent);
+	}
+
+	private void initializeViews() {
+		barivataEditText = findViewById(R.id.editText);
 	}
 }
