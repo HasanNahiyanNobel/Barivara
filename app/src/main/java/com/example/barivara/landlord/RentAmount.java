22c5704f0a60c4e290eab83c9f4902c2617fff5d
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-18 11:38:08 +0600

    Halfway through sending post request of houses.

--- app/src/main/java/com/example/barivara/landlord/RentAmount.java
+++ app/src/main/java/com/example/barivara/landlord/RentAmount.java
@@ -2,13 +2,26 @@ package com.example.barivara.landlord;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.barivara.MainActivity;
 import com.example.barivara.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -29,12 +42,75 @@ public class RentAmount extends AppCompatActivity {
 
 	public void finishAddingHouse(View view) {
 		/**
-		 * Finishes the proccess of adding a house, and goes to Landlord's main page.
+		 * Finishes the process of adding a house, and goes to Landlord's main page.
 		 */
 		HouseAddress.newHouse.setBarivara(Integer.parseInt(barivataEditText.getText().toString()));
 
+		new HTTPAsyncTask().execute(getString(R.string.server_and_port)+getString(R.string.server_house_data));
+
 		Intent intent = new Intent(this, MainPage.class);
 		startActivity(intent);
+	}
+
+	private class HTTPAsyncTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			// params comes from the execute() call: params[0] is the url.
+			try {
+				try {
+					return HttpPost(urls[0]);
+				} catch (JSONException e) {
+					e.printStackTrace();
+					return "Error!";
+				}
+			} catch (IOException e) {
+				return "Unable to retrieve web page. URL may be invalid.";
+			}
+		}
+		/*// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			tvResult.setText(result);
+		}*/
+	}
+
+	private String HttpPost(String myUrl) throws IOException, JSONException {
+		URL url = new URL(myUrl);
+
+		// 1. create HttpURLConnection
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+		// 2. build JSON object
+		JSONObject jsonObject = buildJsonObject();
+
+		// 3. add JSON content to POST request body
+		setPostRequestContent(conn, jsonObject);
+
+		// 4. make POST request to the given URL
+		conn.connect();
+
+		// 5. return response message
+		return conn.getResponseMessage()+"";
+	}
+
+	private JSONObject buildJsonObject() throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.accumulate("name", nameEditText.getText().toString());
+		jsonObject.accumulate("email",  emailEditText.getText().toString());
+		jsonObject.accumulate("password",  passwordEditText.getText().toString());
+		return jsonObject;
+	}
+
+	private void setPostRequestContent(HttpURLConnection conn, JSONObject jsonObject) throws IOException {
+		OutputStream os = conn.getOutputStream();
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+		writer.write(jsonObject.toString());
+		Log.i(MainActivity.class.toString(), jsonObject.toString());
+		writer.flush();
+		writer.close();
+		os.close();
 	}
 
 	private void initializeViews() {
